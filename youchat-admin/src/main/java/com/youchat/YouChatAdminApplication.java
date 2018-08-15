cff2aafef7b4d1ec2247eeb0203fc443414f2028
lienbo <lienbo@zhuanzhuan.com>
2018-08-15 17:02:39 +0800

    add some ori pro

---
+++ youchat-admin/src/main/java/com/youchat/YouChatAdminApplication.java
@@ -0,0 +1,23 @@
+package com.youchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+
+/**
+ * @program: youchat-common
+ * @description: main
+ * @author: lien6o
+ * @create: 2018-08-15 16:01
+ **/
+
+@ServletComponentScan
+@SpringBootApplication
+@EnableCaching
+public class YouChatAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YouChatAdminApplication.class, args);
+    }
+}
---
+++ youchat-websocket/src/main/java/com/youchat/websocket/YouChatWebSocketApplication.java
@@ -0,0 +1,15 @@
+package com.youchat.websocket;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @program: youchat-common
+ * @description: main
+ * @author: lien6o
+ * @create: 2018-08-14 19:07
+ **/
+public class YouChatWebSocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YouChatWebSocketApplication.class, args);
+    }
+}
