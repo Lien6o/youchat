2f2db46889865d3b4a68c38000c7171953beca4f
lienbo <lienbo@zhuanzhuan.com>
2018-08-16 10:14:19 +0800

    add

---
+++ youchat-common/src/main/java/com/youchat/common/cache/DisruptorCache.java
@@ -0,0 +1,11 @@
+package com.youchat.common.cache;
+
+/**
+ * @program: youchat-common
+ * @description:
+ * @author: lien6o
+ * @create: 2018-08-15 17:48
+ **/
+public class DisruptorCache<A, V> {
+ 
+}
