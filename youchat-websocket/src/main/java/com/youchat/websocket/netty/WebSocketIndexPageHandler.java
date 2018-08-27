6b948f6d77bb0d57e9f498025656520b63f708eb
lienbo <lienbo@zhuanzhuan.com>
2018-08-27 17:25:14 +0800

    add websocket demo form io.netty

---
+++ youchat-websocket/src/main/java/com/youchat/websocket/netty/WebSocketIndexPageHandler.java
@@ -0,0 +1,79 @@
+package com.youchat.websocket.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.util.CharsetUtil;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+/**
+ * @program: youchat-common
+ * @description: 入站
+ * @author: lien6o
+ * @create: 2018-08-27 16:52
+ **/
+public class WebSocketIndexPageHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+
+     private final String websocketPath;
+
+    public WebSocketIndexPageHandler(String websocketPath) {
+        this.websocketPath = websocketPath;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest req) throws Exception {
+        if (!req.decoderResult().isSuccess()) {
+            sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
+        }
+
+
+        // Send the index page
+        if ("/".equals(req.uri()) || "/index.html".equals(req.uri())) {
+            String webSocketLocation = getWebSocketLocation(ctx.pipeline(), req, websocketPath);
+            ByteBuf content = WebSocketServerIndexPage.getContent(webSocketLocation);
+            FullHttpResponse res = new DefaultFullHttpResponse(HTTP_1_1, OK, content);
+
+            res.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/html; charset=UTF-8");
+            HttpUtil.setContentLength(res, content.readableBytes());
+
+            sendHttpResponse(ctx, req, res);
+        } else {
+            sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HTTP_1_1, NOT_FOUND));
+        }
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+    private String getWebSocketLocation(ChannelPipeline pipeline, FullHttpRequest req, String websocketPath) {
+        String protocol = "ws";
+        if (pipeline.get(SslHandler.class) != null) {
+            protocol = "wss";
+        }
+        return protocol + "://" + req.headers().get(HttpHeaderNames.HOST) + websocketPath;
+    }
+
+    private static void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, FullHttpResponse res) {
+        // Generate an error page if response getStatus code is not OK (200).
+        if (res.status().code() != 200) {
+            ByteBuf buf = Unpooled.copiedBuffer(res.status().toString(), CharsetUtil.UTF_8);
+            res.content().writeBytes(buf);
+            buf.release();
+            HttpUtil.setContentLength(res, res.content().readableBytes());
+        }
+
+        // Send the response and close the connection if necessary.
+        ChannelFuture f = ctx.channel().writeAndFlush(res);
+        if (!HttpUtil.isKeepAlive(req) || res.status().code() != 200) {
+            f.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+}
