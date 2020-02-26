6512bf9bb9269cada27572e152cbdebe141eaffd
lienbo <lienbo@zhuanzhuan.com>
2020-02-26 10:12:57 +0800

    disruptor and netty

---
+++ youchat-common/src/main/java/com/youchat/common/clone/CloneTest.java
@@ -0,0 +1,100 @@
+package com.youchat.common.clone;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+/**
+ * @author: Lien6o
+ * @description:
+ * @date: 2020/2/25 11:33 上午
+ * @version: v1.0
+ */
+
+//定义学生类
+    @Data
+class Student implements Cloneable{
+    private String name; //学生姓名
+    private Teacher teacher; //定义老师类
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Student clone() {
+        Student student = null;
+        try {
+            student = (Student) super.clone();
+//            Teacher teacher = this.teacher.clone();//克隆teacher对象
+          //  student.setTeacher(teacher);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return student;
+    }
+
+}
+
+//定义老师类
+@Data
+class Teacher implements Cloneable{
+    private String name;  //老师姓名
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name= name;
+    }
+
+    //重写克隆方法，堆老师类进行克隆
+    public Teacher clone() {
+        Teacher teacher= null;
+        try {
+            teacher= (Teacher) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return teacher;
+    }
+}
+
+  class Test {
+
+    public static void main(String args[]) {
+        Teacher teacher = new Teacher (); //定义老师1
+        teacher.setName("刘老师");
+        Student stu1 = new Student();  //定义学生1
+        stu1.setName("test1");
+        stu1.setTeacher(teacher);
+
+        Student stu2 = stu1.clone(); //定义学生2
+        stu2.setName("test2");
+        stu2.getTeacher().setName("王老师");//修改老师
+        System.out.println("学生" + stu1.getName() + "的老师是:" + stu1.getTeacher().getName());
+        System.out.println("学生" + stu1.getName() + "的老师是:" + stu2.getTeacher().getName());
+        System.out.println(LocalDateTime.now());
+        for (int i = 0; i < 10000000; i++) {
+            Student student = new Student();
+        }
+        System.out.println(LocalDateTime.now());
+        Student student = new Student();
+        for (int i = 0; i < 10000000; i++) {
+            Student clone = student.clone();
+        }
+        System.out.println(LocalDateTime.now());
+    }
+}
---
+++ youchat-common/src/main/java/com/youchat/common/sort/LowLevelSort.java
@@ -0,0 +1,88 @@
+package com.youchat.common.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author: Lien6o
+ * @description:
+ * @date: 2020/2/25 6:29 下午
+ * @version: v1.0
+ */
+public class LowLevelSort {
+
+    private static int[] array = {6, 35, 23, 1, 67, 20, 9, 18};
+
+    public static int[] insertSort(int[] array) {
+        int length = array.length;
+        // 次数控制
+        for (int i = 0; i < length; i++) {
+
+            // 和前面的对比 假设前面都是排好的
+            for (int j = i; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    // swap
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+        return array;
+    }
+
+
+
+    public static int[] selectSort(int[] array) {
+        int length = array.length;
+        for (int j = 0; j < length; j++) {
+            int min = array[j];
+            int curr = j;
+            // 找最小
+            for (int i = j; i < length; i++) {
+                if (min > array[i] ) {
+                    min = array[i];
+                    curr = i;
+                }
+            }
+            // swap 交换
+            int temp = array[j];
+            array[j] = min;
+            array[curr] = temp;
+        }
+        return array;
+    }
+
+
+    public static int[] bubbleSort(int[] array) {
+        int length = array.length;
+        // 提前退出冒泡循环的标志位
+        boolean flag = false;
+        // 外层控制次数
+        for (int j = 0; j < length; j++) {
+            // 内层是一次所有的两两互换  大的数字一点一点的向后移动 像不像水泡从水底冒出。。。哈哈哈哈
+            for (int i = 0; i < length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = temp;
+                    flag = true; // 表示有数据交换
+                }
+            }
+            if (!flag) break; // 没有数据交换，提前退出
+        }
+        return array;
+    }
+
+
+
+
+    public static void main(String[] args) {
+       System.out.println("bubbleSort "+Arrays.toString(insertSort(array)));
+        //  System.out.println("selectSort "+ Arrays.toString(insertSort(array)));
+    }
+
+
+
+}
