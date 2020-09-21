df6759184e8759bdf8bb4babd339577588f43a02
lienbo <lienbo@zhuanzhuan.com>
2020-09-21 20:47:22 +0800

    add md

--- youchat-common/src/main/java/com/youchat/common/leetcode/SpiralOrder.java
+++ youchat-common/src/main/java/com/youchat/common/leetcode/SpiralOrder.java
@@ -50,20 +50,22 @@ public class SpiralOrder {
 
     public static void main(String[] args) {
         int[][] matrix = {
-                {1, 2, 3, 4, 5},
-                {14, 15, 16, 17, 6},
-                {13, 20, 19, 18, 7},
-                {12, 11, 10, 9, 8}
+                { 1,  2,  3,  4, 5 },
+                {14, 15, 16, 17, 6 },
+                {13, 20, 19, 18, 7 },
+                {12, 11, 10,  9, 8 }
         };
         int[][] matrix2 = {
-                {1, 2, 3, 4, 5},
-                {16, 17,18, 19, 6},
-                {15, 24, 25, 20, 7},
-                {14, 23, 22, 21, 8},
-                {13, 12, 11, 10, 9}
+                {1,   2,  3,  4, 5 },
+                {16, 17, 18, 19, 6 },
+                {15, 24, 25, 20, 7 },
+                {14, 23, 22, 21, 8 },
+                {13, 12, 11, 10, 9 }
         };
+
         List<Integer> resultList1 = spiralOrder(matrix);
         System.out.println(Arrays.toString(resultList1.toArray()));
+
         List<Integer> resultList2 = spiralOrder(matrix2);
         System.out.println(Arrays.toString(resultList2.toArray()));
     }
