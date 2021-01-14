4301b87bd95245442ba2a3b16939b7f45a4509e1
lienbo <lienbo@meituan.com>
2021-01-14 20:29:48 +0800

    7788

--- youchat-common/src/main/java/com/youchat/common/pipeline/StrategyMain.java
+++ youchat-common/src/main/java/com/youchat/common/pipeline/StrategyMain.java
@@ -9,15 +9,17 @@ import java.util.function.Predicate;
 
 public class StrategyMain {
 
+    /**
+     * 这个例子中策略模式有明显的链式的规则
+     * 但是使用List也可以很好地反应这种规则
+     * 类似Spring的DispatchServlet中的各种Resolver等也是List组织的
+     */
+    public static List<Integer> filterPowerByStrategy(List<String> allPowers, PowerQuery powerQuery) {
+        List<Pair<Predicate<PowerQuery>, BiFunction<List<String>, PowerQuery, List<Integer>>>> chains = new ArrayList<>();
 
-    public static List<String> filterPowerByStrategy(List<String> allPowers, PowerQuery powerQuery) {
-        // 这个例子中策略模式有明显的链式的规则
-        // 但是使用List也可以很好地反应这种规则
-        // 类似Spring的DispatchServlet中的各种Resolver等也是List组织的
-        List<Pair<Predicate<PowerQuery>, BiFunction<List<String>, PowerQuery, List<String>>>> chains = new ArrayList<>();
         // ALL的逻辑
         chains.add(
-                Pair.of("逻辑1", query -> "ALL".equalsIgnoreCase(query.getType()), (powers, query) -> powers)
+                Pair.of("逻辑1", query -> "ALL".equalsIgnoreCase(query.getType()), (powers, query) -> Lists.newArrayList(0))
         );
 
         // 这里将外部用户的逻辑提到上部
@@ -25,7 +27,7 @@ public class StrategyMain {
                 Pair.of("逻辑2", query -> new Integer(1).equals(query.getUserType()), (powers, query) ->
                         {
                             powers.add("333");
-                            return powers;
+                            return Lists.newArrayList(1,2,3);
                         }
                 )
         );
@@ -35,33 +37,35 @@ public class StrategyMain {
                 Pair.of("逻辑3", query -> new Integer(0).equals(query.getUserType()), (powers, query) ->
                         {
                             powers.add(query.getType());
-                            return powers;
+                            int a = 1/0;
+                            return Lists.newArrayList(3,4,3);
                         }
                 )
         );
 
         //最后增加一个收尾的策略 其他情况统一返回原全量权限
         chains.add(
-                Pair.of("默认逻辑", query -> true, (powers, query) -> powers)
+                Pair.of("默认逻辑", query -> true, (powers, query) -> new ArrayList<>())
         );
 
         // 使用策略List
-        for (Pair<Predicate<PowerQuery>, BiFunction<List<String>, PowerQuery, List<String>>> chain : chains) {
-            System.out.println("strategy = " + chain.getName());
+        for (Pair<Predicate<PowerQuery>, BiFunction<List<String>, PowerQuery, List<Integer>>> chain : chains) {
             if (chain.getPredicate().test(powerQuery)) {
+                System.out.println("strategy = " + chain.getName());
                 return chain.getFunction().apply(allPowers, powerQuery);
             }
         }
+
         // 这个逻辑是不会走的
-        return allPowers;
+        return new ArrayList<>();
     }
 
     public static void main(String[] args) {
         PowerQuery powerQuery = new PowerQuery();
-       // powerQuery.setType("ALL7");
-       // powerQuery.setUserType(0);
+        // powerQuery.setType("ALL7");
+        powerQuery.setUserType(0);
 
-        List<String> strings = filterPowerByStrategy(Lists.newArrayList("1", "2", "3"), powerQuery);
+        List<Integer> strings = filterPowerByStrategy(Lists.newArrayList("1", "2", "3"), powerQuery);
         System.out.println("strings = " + strings);
     }
 }
